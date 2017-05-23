5bb5c74471fe5b678ff315f682ef13afa170cce9
TULSACOUNTY\jholland <jholland@tulsacounty.org>
2017-05-23 15:00:04 -0500

    Add demo test

---
+++ WebApplication1/WebApplication1.Tests/DemoTests1.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Tests
+{
+    [TestFixture]
+    public class DemoTests1
+    {
+        [Test]
+        public void Should_test_something()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
